e9f5398a720fb61264e2d40649f22fa9e3e64461
Sneha Gupta <snehagupta@Snehas-MacBook-Pro.local>
2021-03-12 17:33:37 +0100

    Update the documentation

---
+++ src/main/reverse_integer.py
@@ -0,0 +1,12 @@
+# Given a signed 32-bit integer x, return x with its digits reversed.
+# If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+# Constraints:
+# -2^31 <= x <= 2^31 - 1
+
+def reverse(x):
+    return 0
+
+
+if __name__ == '__main__':
+    print(reverse(10))
