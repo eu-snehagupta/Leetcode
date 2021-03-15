eadc19283ae6864467f1aa58ca9ad93590fe1717
Sneha Gupta <eu.snehagupta@gmail.com>
2021-03-15 13:42:59 +0100

    Push improved solution for valid parentheses question

--- src/main/valid_parentheses.py
+++ src/main/valid_parentheses.py
@@ -17,29 +17,35 @@
 # s consists of parentheses only '()[]{}'.
 
 
-def is_valid_parentheses(input_string):
-    if len(input_string) == 0 or len(input_string) % 2 != 0:
-        return False
-    is_valid = False
-    parentheses_queue = []
-    for element in input_string:
-        if element in "({[":
-            parentheses_queue.append(element)
-            is_valid = False
+def is_valid_parentheses(s):
+    # The stack to keep track of opening brackets.
+    stack = []
+
+    # Hash map for keeping track of mappings. This keeps the code very clean.
+    # Also makes adding more types of parenthesis easier
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    # For every bracket in the expression.
+    for char in s:
+
+        # If the character is an closing bracket
+        if char in mapping:
+
+            # Pop the topmost element from the stack, if it is non empty
+            # Otherwise assign a dummy value of '#' to the top_element variable
+            top_element = stack.pop() if stack else '#'
+
+            # The mapping for the opening bracket in our hash and the top
+            # element of the stack don't match, return False
+            if mapping[char] != top_element:
+                return False
         else:
-            if len(parentheses_queue) == 0:
-                return False
-            if element == ")" and parentheses_queue.pop() == "(":
-                is_valid = True
-            elif element == "}" and parentheses_queue.pop() == "{":
-                is_valid = True
-            elif element == "]" and parentheses_queue.pop() == "[":
-                is_valid = True
-            else:
-                return False
-    if len(parentheses_queue) != 0:
-        return False
-    return is_valid
+            # We have an opening bracket, simply push it onto the stack.
+            stack.append(char)
+
+    # In the end, if the stack is empty, then we have a valid expression.
+    # The stack won't be empty for cases like ((()
+    return not stack
 
 
 if __name__ == '__main__':
