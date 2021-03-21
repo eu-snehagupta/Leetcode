4c758fdc5870754658c948c99527f0be258b4cde
Sneha Gupta <eu.snehagupta@gmail.com>
2021-03-22 00:29:59 +0100

    Add documentatio

--- src/main/remove_duplicates.py
+++ src/main/remove_duplicates.py
@@ -1,3 +1,24 @@
+# Given a sorted array nums, remove the duplicates in-place
+# such that each element appears only once and returns the new length.
+# Do not allocate extra space for another array, you must do this by
+# modifying the input array in-place with O(1) extra memory.
+#
+# Clarification:
+# Confused why the returned value is an integer but your answer is an array?
+# Note that the input array is passed in by reference, which means a modification
+# to the input array will be known to the caller as well.
+#
+# Example 1:
+# Input: nums = [1,1,2]
+# Output: 2, nums = [1,2]
+# Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+# It doesnt matter what you leave beyond the returned length.
+#
+# Constraints:
+# 0 <= nums.length <= 3 * 10**4
+# -10**4 <= nums[i] <= 10**4
+# nums is sorted in ascending order.
+
 def remove_duplicates(input_list):
     if len(input_list) <= 1:
         return len(input_list)
--- src/main/remove_element.py
+++ src/main/remove_element.py
@@ -1,3 +1,26 @@
+# Given an array nums and a value val, remove all instances
+# of that value in-place and return the new length.
+# Do not allocate extra space for another array, you must do
+# this by modifying the input array in-place with O(1) extra memory.
+# The order of elements can be changed. It doesnt matter what you leave beyond the new length.
+#
+# Clarification:
+# Confused why the returned value is an integer but your answer is an array?
+# Note that the input array is passed in by reference, which means a modification
+# to the input array will be known to the caller as well.
+#
+# Example 1:
+# Input: nums = [3,2,2,3], val = 3
+# Output: 2, nums = [2,2]
+# Explanation: Your function should return length = 2, with the first two elements of nums being 2.
+# It doesnt matter what you leave beyond the returned length.
+# For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
+#
+# Constraints:
+# 0 <= nums.length <= 100
+# 0 <= nums[i] <= 50
+# 0 <= val <= 100
+
 def remove_element(input_list, value):
     if not input_list:
         return 0
